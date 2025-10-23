6acb14b5bd4e69afffc7738111f2d33d3c139d65
Samannyo Pal <samannyopal04@gmail.com>
2025-10-24 01:54:35 +0530

    readme

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 // Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
-//#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use std::{cell::RefCell, collections::HashMap, error::Error, os::windows::process, rc::Rc, thread, time::Duration};
 use slint::{ComponentHandle, SharedString, VecModel, ModelRc};
